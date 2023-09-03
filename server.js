d6d4bc73fbdc4c3a8bcabf018c5798b54afbd9fc
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-09-03 11:26:52 +0700

    Feat: Merge from Loi

--- server.js
+++ server.js
@@ -1,73 +1,73 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const colors = require("colors");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const compression = require("compression");
-const helmet = require("helmet");
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const compression = require('compression')
+const helmet = require('helmet')
 
-const errorHandler = require("./middleware/error.middleware");
-const connectDB = require("./configs/db.config");
+const errorHandler = require('./middleware/error.middleware')
+const connectDB = require('./configs/db.config')
 
-const app = express();
+const app = express()
 
 // Body Parser
-app.use(express.json());
+app.use(express.json())
 
 // Cookie Parser
-app.use(cookieParser());
+app.use(cookieParser())
 
 // Config security headers
-app.use(helmet());
+app.use(helmet())
 
 /**
  * Reduce the size of files being sent from the server to the user's browser,
  * Thereby improving page load speed and reducing network bandwidth usage
  */
-app.use(compression());
+app.use(compression())
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-const users = require("./modules/user/user.route");
-const auth = require("./modules/auth/auth.route");
-const category = require("./modules/category/category.route");
-const product = require("./modules/product/product.route");
-const cart = require("./modules/cart/cart.route");
-const order = require("./modules/order/order.route");
-const system = require("./modules/system/system.route");
+const users = require('./modules/user/user.route')
+const auth = require('./modules/auth/auth.route')
+const category = require('./modules/category/category.route')
+const product = require('./modules/product/product.route')
+const cart = require('./modules/cart/cart.route')
+const order = require('./modules/order/order.route')
+const system = require('./modules/system/system.route')
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(`/api/${process.env.API_VERSION_1}/users`, users);
-app.use(`/api/${process.env.API_VERSION_1}/auth`, auth);
-app.use(`/api/${process.env.API_VERSION_1}/categories`, category);
-app.use(`/api/${process.env.API_VERSION_1}/carts`, cart);
-app.use(`/api/${process.env.API_VERSION_1}/products`, product);
-app.use(`/api/${process.env.API_VERSION_1}/orders`, order);
-app.use(`/api/${process.env.API_VERSION_1}/system`, system);
+app.use(`/api/${process.env.API_VERSION_1}/users`, users)
+app.use(`/api/${process.env.API_VERSION_1}/auth`, auth)
+app.use(`/api/${process.env.API_VERSION_1}/categories`, category)
+app.use(`/api/${process.env.API_VERSION_1}/carts`, cart)
+app.use(`/api/${process.env.API_VERSION_1}/products`, product)
+app.use(`/api/${process.env.API_VERSION_1}/orders`, order)
+app.use(`/api/${process.env.API_VERSION_1}/system`, system)
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
   console.log(
     `Server connected to http://localhost:${PORT}`.magenta.underline.bold
-  );
-});
+  )
+})
 
 // Handle the unhandled promise rejection
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
 
   // Close server | Exit a process
-  server.close(() => process.exit(1));
-});
+  server.close(() => process.exit(1))
+})
