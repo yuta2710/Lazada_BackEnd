2d7d8babd2c5fe78a73999bc2e7d672690c067ae
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-09-03 09:58:39 +0700

    initial branch Khoi

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,7 +1,7 @@
-const asyncHandler = require("../../middleware/async.middleware");
-const ErrorResponse = require("../../utils/error.util");
-const { createToken } = require("../../utils/token.util");
-const userModel = require("../user/user.model");
+const asyncHandler = require('../../middleware/async.middleware')
+const ErrorResponse = require('../../utils/error.util')
+const { createToken } = require('../../utils/token.util')
+const userModel = require('../user/user.model')
 
 /**
  * @des:     Register user
@@ -10,16 +10,16 @@ const userModel = require("../user/user.model");
  */
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, phone_number, password, business, address, role } =
-    req.body;
+    req.body
 
   console.table([
-    { name, email, phone_number, password, business, address, role },
-  ]);
+    { name, email, phone_number, password, business, address, role }
+  ])
 
-  let user;
+  let user
 
   if (!(await userModel.findOne({ phone_number: phone_number }))) {
-    if (role === "seller" || business) {
+    if (role === 'seller' || business) {
       user = await userModel.create({
         name,
         email,
@@ -28,10 +28,10 @@ exports.register = asyncHandler(async (req, res, next) => {
         address,
         role,
         business,
-        status: "Pending",
-      });
+        status: 'Pending'
+      })
       // user.set("business", business);
-      await user.save();
+      await user.save()
     } else {
       user = await userModel.create({
         name,
@@ -39,23 +39,23 @@ exports.register = asyncHandler(async (req, res, next) => {
         phone_number,
         password,
         address,
-        role,
-      });
+        role
+      })
     }
   } else {
     res.status(400).json({
       success: false,
-      message: "Duplicate error. A phone-number should be unique",
-    });
+      message: 'Duplicate error. A phone-number should be unique'
+    })
   }
 
-  const token = await createToken(user);
+  const token = await createToken(user)
 
   res.status(201).json({
     success: true,
-    token,
-  });
-});
+    token
+  })
+})
 
 /**
  * @des:     Login user
@@ -63,9 +63,9 @@ exports.register = asyncHandler(async (req, res, next) => {
  * @access:  Public
  */
 exports.login = asyncHandler(async (req, res, next) => {
-  const { identifier, password } = req.body;
+  const { identifier, password } = req.body
 
-  console.table({ identifier, password });
+  console.table({ identifier, password })
   // Validate email and password
   if (!identifier || !password) {
     return next(
@@ -73,52 +73,52 @@ exports.login = asyncHandler(async (req, res, next) => {
         400,
         `Please provide the email/phone-number and password`
       )
-    );
+    )
   }
 
-  let user;
+  let user
   const regexEmail =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  const regexPhoneNumber = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+  const regexPhoneNumber = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
 
   if (regexEmail.test(identifier)) {
-    user = await userModel.findOne({ email: identifier }).select("+password");
+    user = await userModel.findOne({ email: identifier }).select('+password')
   }
 
   if (regexPhoneNumber.test(identifier)) {
     user = await userModel
       .findOne({ phone_number: identifier })
-      .select("+password");
+      .select('+password')
   }
 
   // Check for user
   if (!user) {
-    return next(new ErrorResponse(401, `Invalid credentials`));
+    return next(new ErrorResponse(401, `Invalid credentials`))
   }
 
   // Check if password matches
-  const isMatch = await user.matchPassword(password);
+  const isMatch = await user.matchPassword(password)
 
   if (!isMatch) {
-    return next(new ErrorResponse(401, `Invalid credentials`));
+    return next(new ErrorResponse(401, `Invalid credentials`))
   }
 
-  const token = createToken(user);
+  const token = createToken(user)
 
   res
     .status(200)
-    .cookie("token", token, {
+    .cookie('token', token, {
       httpOnly: true,
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      )
     })
     .json({
       success: true,
-      token,
-    });
-});
+      token
+    })
+})
 
 /**
  * @des:     Get me
@@ -126,22 +126,22 @@ exports.login = asyncHandler(async (req, res, next) => {
  * @access:  Private
  */
 exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await userModel.findById(req.user.id);
+  const user = await userModel.findById(req.user.id)
 
   res.status(200).json({
     success: true,
-    data: user,
-  });
-});
+    data: user
+  })
+})
 
 exports.logOut = asyncHandler(async (req, res, next) => {
-  res.cookie("token", "none", {
+  res.cookie('token', 'none', {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
+    httpOnly: true
+  })
 
   res.status(200).json({
     success: true,
-    data: {},
-  });
-});
+    data: {}
+  })
+})
